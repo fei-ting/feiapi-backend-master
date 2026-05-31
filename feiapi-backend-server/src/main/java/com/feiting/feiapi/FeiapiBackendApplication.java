5933a933030db9bcde973d90559d1108f50abdea
feiting <785438525@qq.com>
2026-05-31 18:33:49 +0800

    fix：修改命名不规范

--- feiapi-backend-server/src/main/java/com/feiting/feiapi/FeiapiBackendApplication.java
+++ feiapi-backend-server/src/main/java/com/feiting/feiapi/FeiapiBackendApplication.java
@@ -1,11 +1,13 @@
 package com.feiting.feiapi;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDubbo
+@MapperScan("com.feiting.feiapi.mapper")
 public class FeiapiBackendApplication {
 
     public static void main(String[] args) {
